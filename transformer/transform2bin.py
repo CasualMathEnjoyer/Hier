5f2ac9380f034efb075d9c6305ecdea41bcb5f8f
CasualMathEnjoyer <morovkat@cvut.cz>
2023-11-08 13:48:12 +0100

    classified the code

--- transformer/transform2bin.py
+++ transformer/transform2bin.py
@@ -2,7 +2,7 @@
 import numpy as np
 import random
 from model_file import model_func
-from keras.models import load_model
+import keras
 from keras.utils import set_random_seed
 from keras import backend as K
 
@@ -21,105 +21,129 @@ print("seed = ", a)
 
 # False Positive = false alarm -> wanted to space it but there shouldnt be a space
 # False Negative = missed space -> should be spaced but it didnt
-def model_test(sample : list, model_name, n, sent_len, embed_dim, slovnik:dict, mezera, sep):
-    model = load_model(model_name, custom_objects={"F1_score": F1_score})
-    # TODO DOWN
-    sample_v = tokenize(sample, dict_chars)
-    value = model.predict(sample_v)  # has to be in the shape of the input for it to predict
 
-    assert len(value) == len(sample_v)
-    # print(value)
-    for j in range(n):
-        for num in value[j]:
-            if num[0] > 0.5:
-                print(1, end=mezera)
-            else:
-                print(0, end=sep)
-        print('')
+training_file_name = "../data/src-sep-train.txt"
+validation_file_name = "../data/src-sep-val.txt"
+model_file_name = "transform2bin_4"
+sep = ' '
+mezera = '_'
 
-        for i, char in enumerate(sample[j]):
-            print(char, end=sep)
-            if value[j][i][0] > 0.5:
-                print(mezera, end=sep)
-            i+=1
+batch_size = 64
+epochs = 1
+repeat = 1  # full epoch_num=epochs*repeat
 
-        print('')
-def sliding_window(output_file: str, sep, space):
-    window, step = 64, 20
+class Data():
+    vocab_size = 1138
+    maxlen = 64
+    embed_dim = 32  # Embedding size for each token
+    num_heads = 2  # Number of attention heads
+    ff_dim = 64  # Hidden layer size in feed forward network inside transformer
 
-    if sep != '':
-        output_file = output_file.split(sep)
-    l = len(output_file)
+    def __init__(self, final_file, valid_file, sep, mezera, dict_chars=None):
+        super().__init__()
+        if dict_chars is None:
+            dict_chars = {}
+        self.final_file = final_file
+        self.valid_file = valid_file
+        self.sep = sep
+        self.mezera = mezera
+        self.dict_chars = dict_chars
 
-    re_windowed, re_binar, list_chars = [], [], []
-    slide = 0
-    num_space = 0
-    num_nonspaces = 0
-    while True:
-        pos, skipped = 0, 0
-        line, line_n = [], []
-        # line.append('<bos>')
-        while pos < window + skipped:  # slide
-            element = output_file[slide * step + pos]
-            assert element != ''
-            if element != space:
-                if "\n" in element:
-                    element = element.replace("\n", "")
-                if element not in list_chars:
-                    list_chars.append(element)
-                line.append(element)
-                re_binar.append(0)
-                num_nonspaces += 1
-            else:  # element was a space
-                skipped += 1
-                re_binar.pop()
-                re_binar.append(1)
-                num_space += 1
-            pos += 1
-        # line.append('<eos>')
-        re_windowed.append(line)
-        # I take a look at the last like without setting pos to 0 and it is too much so it stops
-        slide += 1
-        if slide * step + pos > l:
-            # print ("spaces:", num_space)
-            # print ("non spaces :", num_nonspaces)
-            break
+    def tokenize(self, input_list):
+        out = np.zeros((len(input_list), 64))
+        unk_counter = 0
+        # assert self.dict_chars != None
+        for i, line in enumerate(input_list):
+            l = np.zeros((64))
+            for j, c in enumerate(line):
+                try:
+                    l[j] = self.dict_chars[c]
+                except KeyError:
+                    l[j] = self.dict_chars["OOV"]
+                    unk_counter += 1
+            out[i] = l
+        print("unknown chars in text: ", unk_counter)
+        return(out)
+    def model_test(self, sample : list, model_name, n, sent_len, embed_dim, slovnik:dict, mezera, sep):
+        model = load_model_mine(model_name)
+        # TODO DOWN
+        sample_v = self.tokenize(sample)
+        value = model.predict(sample_v)  # has to be in the shape of the input for it to predict
 
-    list_chars.append('OOV')
-    dict_chars = {j: i for i, j in enumerate(list_chars)}
+        assert len(value) == len(sample_v)
+        # print(value)
+        for j in range(n):
+            for num in value[j]:
+                if num[0] > 0.5:
+                    print(1, end=mezera)
+                else:
+                    print(0, end=sep)
+            print('')
 
-    num_line = len(re_windowed)
-    re_binar = np.array(re_binar)
-    re_binar = np.reshape(re_binar, (num_line, window))
-    # print(len(re_binar))
-    # print(len(re_binar[0]))
-    # for i in range(len(re_windowed)):
-    #     # print(re_windowed[i])
-    #     # print(re_binar[i])
-    #     for j, char in enumerate(re_windowed[i]):
-    #         print(char, end=sep)
-    #         if re_binar[i][j] == 1:
-    #             print(space, end=sep)
-    #     print('')
+            for i, char in enumerate(sample[j]):
+                print(char, end=sep)
+                if value[j][i][0] > 0.5:
+                    print(mezera, end=sep)
+                i+=1
 
-    assert len(re_binar) == len(re_windowed)
-    assert len(re_binar[0]) == len(re_windowed[0])
+            print('')
+    def sliding_window(self, output_file: str):
+        window, step = 64, 20
+        sep = self.sep
+        space = self.mezera
 
-    return re_windowed, re_binar, dict_chars
-def tokenize(input_list, dict_chars):
-    out = np.zeros((len(input_list), 64))
-    unk_counter = 0
-    for i, line in enumerate(input_list):
-        l = np.zeros((64))
-        for j, c in enumerate(line):
-            try:
-                l[j] = dict_chars[c]
-            except KeyError:
-                l[j] = dict_chars["OOV"]
-                unk_counter += 1
-        out[i] = l
-    print("unknown chars in text: ", unk_counter)
-    return(out)
+        if sep != '':
+            output_file = output_file.split(sep)
+        l = len(output_file)
+
+        re_windowed, re_binar, list_chars = [], [], []
+        list_chars.append('OOV')
+
+        slide, num_space, num_nonspaces= 0, 0, 0
+        while True:
+            pos, skipped = 0, 0
+            line, line_n = [], []
+            # line.append('<bos>')
+            while pos < window + skipped:  # slide
+                element = output_file[slide * step + pos]
+                assert element != ''
+                if element != space:
+                    if "\n" in element:
+                        element = element.replace("\n", "")
+                    if element not in list_chars:
+                        list_chars.append(element)
+                    line.append(element)
+                    re_binar.append(0)
+                    num_nonspaces += 1
+                else:  # element was a space
+                    skipped += 1
+                    re_binar.pop()
+                    re_binar.append(1)
+                    num_space += 1
+                pos += 1
+            # line.append('<eos>')
+            re_windowed.append(line)
+            # I take a look at the last like without setting pos to 0 and it is too much so it stops
+            slide += 1
+            if slide * step + pos > l:
+                # print ("spaces:", num_space)
+                # print ("non spaces :", num_nonspaces)
+                break
+
+        dict_chars = {j: i for i, j in enumerate(list_chars)}
+
+        num_line = len(re_windowed)
+        re_binar = np.array(re_binar)
+        re_binar = np.reshape(re_binar, (num_line, window))
+
+        assert len(re_binar) == len(re_windowed)
+        assert len(re_binar[0]) == len(re_windowed[0])
+
+        if not bool(self.dict_chars):  # empty dicts evaluate as false
+            self.dict_chars = dict_chars
+
+        return re_windowed, re_binar, dict_chars
+
 def F1_score(y_true, y_pred): #taken from old keras source code
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
@@ -128,22 +152,8 @@ def F1_score(y_true, y_pred): #taken from old keras source code
     recall = true_positives / (possible_positives + K.epsilon())
     f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
     return f1_val
-
-training_file_name = "../data/src-sep-train.txt"
-validation_file_name = "../data/src-sep-val.txt"
-model_file_name = "transform2bin_4"
-sep = ' '
-mezera = '_'
-
-vocab_size = 1138  # TODO - to be adjusted
-maxlen = 64
-embed_dim = 32  # Embedding size for each token
-num_heads = 2  # Number of attention heads
-ff_dim = 64  # Hidden layer size in feed forward network inside transformer
-
-batch_size = 64
-epochs = 0
-repeat = 1  # full epoch_num=epochs*repeat
+def load_model_mine(model_name):
+    return keras.models.load_model(model_name, custom_objects={"F1_score": F1_score})
 
 # -------------------------------- DATA ---------------------------------------------------------------------------
 print("data preparation...")
@@ -152,19 +162,20 @@ with open(training_file_name, "r", encoding="utf-8") as f:  # with spaces
 with open(validation_file_name, "r", encoding="utf-8") as ff:
     valid_file = ff.read()
 
-x_train, y_train, dict_chars = sliding_window(final_file, sep, mezera)
-x_valid, y_valid, dict_val = sliding_window(valid_file, sep, mezera)
+d = Data(final_file, valid_file, sep, mezera)
 
-x_train_tokenized = tokenize(x_train, dict_chars)
-x_valid_tokenized = tokenize(x_valid, dict_chars)
+x_train, y_train, dict_chars = d.sliding_window(final_file)
+x_valid, y_valid, dict_val = d.sliding_window(valid_file)
+
+x_train_tokenized = d.tokenize(x_train)
+x_valid_tokenized = d.tokenize(x_valid)
 
 # --------------------------------- MODEL ---------------------------------------------------------------------------
 print("model starting...")
 if 0:
-    model = model_func(vocab_size, maxlen, embed_dim, num_heads, ff_dim)
+    model = model_func(d.vocab_size, d.maxlen, d.embed_dim, d.num_heads, d.ff_dim)
 else:
-    from keras.models import load_model
-    model = load_model(model_file_name, custom_objects={"F1_score": F1_score})
+    model = load_model_mine(model_file_name)
 
 model.compile(optimizer="adam", loss="binary_crossentropy",
               metrics=["accuracy", "Precision", "Recall", F1_score])
@@ -174,14 +185,11 @@ for i in range(repeat):
     history = model.fit(
         x_train_tokenized, y_train, batch_size=batch_size, epochs=epochs,
         validation_data=(x_valid_tokenized, y_valid))
-    K.clear_session()
-
-    # print("saving model ...")
     model.save(model_file_name)
-    # print("model saved")
+    K.clear_session()
 
 # ---------------------------------- TESTING ------------------------------------------------------------------------
 print("testing...")
 
-sample, _, _ = sliding_window(final_file[:1000], sep, mezera)
-model_test(sample, model_file_name, len(sample), 64, 32, dict_chars, mezera, sep)
+sample, _, _ = d.sliding_window(final_file[:1000])
+d.model_test(sample, model_file_name, len(sample), 64, 32, dict_chars, mezera, sep)

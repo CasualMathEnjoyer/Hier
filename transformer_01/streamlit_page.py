f31e2ee04e609de64badf156db15207227feab24
CasualMathEnjoyer <morovkat@cvut.cz>
2024-02-20 21:44:39 +0100

    pretty page

--- transformer_01/streamlit_page.py
+++ transformer_01/streamlit_page.py
@@ -14,16 +14,39 @@ ar = []
 
 st.header("The English Model")
 
+# Store the initial value of widgets in session state
+if "visibility" not in st.session_state:
+    st.session_state.visibility = "visible"
+    st.session_state.disabled = False
+    st.session_state.placeholder = "Enter text"
+
+# input box
+text_input = st.text_input(
+        "Enter text 👇",
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+        placeholder=st.session_state.placeholder,
+    )
+
+# todo - load model
+
+# future output box
+text_output = st.text("Changed input:" + text_input)
+
+# GRAPHS
 genre = st.radio(
     "Select metrics",
-    ["loss", "accuracy", "precision&recall", "F1 score"])
+    ["loss", "accuracy", "precision", "recall", "F1 score"])
+
 dict = {
     "loss" : 0,
     "accuracy" : 1,
-    "precision&recall" : 2,
-    "F1 score" : 3
+    "precision" : 2,
+    "recall" : 3,
+    "F1 score" : 4
 }
 
+# i do hate the mess bellow
 for i in range(len(history[keys[dict[genre]]])):
     item = [history[keys[dict[genre]]][i], history[keys[dict[genre]+5]][i]]
     ar.append(item)

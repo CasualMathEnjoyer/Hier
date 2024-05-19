970c7636c1c4ca8b3d646b15e3cf17dd7255f371
CasualMathEnjoyer <morovicsova.katka@gmail.com>
2024-05-19 18:20:46 +0200

    my model without functional attention masking

--- 2_en_de_transformer/model_file_mine.py
+++ 2_en_de_transformer/model_file_mine.py
@@ -1,97 +1,121 @@
 import keras
 import numpy as np
+import tensorflow as tf
+import keras_nlp
 
+# todo missing future masking
+# TODO masking is not propagating - implement it elseway?
+# !!! TODO KerasNLP layers sinecosine positional embeddings !!!
+# todo - I would want the potional encoding to be a simple fixed layer that just does addition - find a way
+# todo - end dimensions are not matching at the possitional encoding layer : [1,2,128] vs. [22,128]
 
-def get_positional_encoding(max_seq_len, d_model):
-    # Initialize the positional encoding matrix with zeros
-    positional_encoding = np.zeros((max_seq_len, d_model))
+class CustomSinePositionEncoding(keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super(CustomSinePositionEncoding, self).__init__(**kwargs)
+        self.sine_position_encoding = keras_nlp.layers.SinePositionEncoding()
 
-    # Calculate the positional encoding values using the given formulas
-    for pos in range(max_seq_len):
-        for i in range(0, d_model, 2):
-            positional_encoding[pos, i] = np.sin(pos / (10000 ** (i / d_model)))
-            positional_encoding[pos, i + 1] = np.cos(pos / (10000 ** ((i + 1) / d_model)))
+    def call(self, inputs, mask=None):
+        positional = self.sine_position_encoding(inputs)
+        return inputs + positional
 
-    return positional_encoding
+    def compute_mask(self, inputs, mask=None):
+        return mask
 
-def possitionalEmbedding(input_dim, output_dim):  # TODO
+class LookAheadMaskLayer(keras.layers.Layer):
+    def call(self, inputs):
+        batch_size, seq_len, _ = tf.shape(inputs)
+        look_ahead_mask = tf.linalg.band_part(tf.ones((seq_len, seq_len)), -1, 0)
+        # look_ahead_mask = tf.reshape(look_ahead_mask, (1, 1, seq_len, seq_len))
+        # return tf.tile(look_ahead_mask, [batch_size, 1, 1, 1])
+        return look_ahead_mask
+    # def compute_mask(self, inputs, mask=None):
+    #     return mask
 
-    return keras.layers.Embedding(input_dim=input_dim, output_dim=output_dim)
 
 def model_func(encoder_vocab_len, decoder_vocab_len, encoder_maxlen, decoder_maxlen, params):
-    num_heads, key_dim, d_v, d_ff, d_model, n = params
+    num_heads, key_dim, value_dim, d_ff, d_model, n = params
 
-    possitional_encoding_encoder = get_positional_encoding(encoder_maxlen, d_model)
-    possitional_encoding_decoder = get_positional_encoding(decoder_maxlen, d_model)
-
-    encoder_input = keras.Input(shape=(encoder_maxlen,))  # fixed len input to apply possitional encoding
+    encoder_input = keras.Input(shape=(encoder_maxlen,))  # fixed len input to apply positional encoding
     decoder_input = keras.Input(shape=(decoder_maxlen,))
 
-    # encoder part
-    embedded = keras.layers.Embedding(input_dim=encoder_vocab_len, output_dim=d_model)(encoder_input)
-    embedded_possition = embedded + possitional_encoding_encoder  # possitional encoding should match the emmbeding shape
-    embedded_possition = keras.layers.Dropout(0.1)(embedded_possition)
+    # Encoder part
+    embedded = keras.layers.Embedding(input_dim=encoder_vocab_len, output_dim=d_model, mask_zero=True)(encoder_input)
+    encoder_mask = embedded._keras_mask
 
-    encoded = embedded_possition
+    embedded_position = CustomSinePositionEncoding()(embedded)
+    embedded_position = keras.layers.Dropout(0.1)(embedded_position)
+
+    encoded = embedded_position
     for i in range(n):
-        attended_encoded = keras.layers.MultiHeadAttention(num_heads,
-                                        key_dim,
-                                        dropout=0.1,
-                                        use_bias=True,
-                                        output_shape=(d_model,))(encoded, encoded, encoded)  # todo padding_mask
+        attended_encoded = keras.layers.MultiHeadAttention(
+            num_heads,
+            key_dim,
+            value_dim=value_dim,
+            dropout=0.1,
+            use_bias=True)(encoded, encoded, encoded,
+                           # attention_mask=encoder_mask
+                           )
         attended_encoded_d = keras.layers.Dropout(0.1)(attended_encoded)
 
         add = encoded + attended_encoded_d
-        normalised = keras.layers.LayerNormalization()(add)
+        normalized = keras.layers.LayerNormalization()(add)
 
-        fed_f = keras.layers.Dense(d_ff)(normalised)  # feed forward 1 part
+        fed_f = keras.layers.Dense(d_ff)(normalized)  # feed forward 1 part
         fed_ff = keras.layers.Dense(d_model)(keras.activations.relu(fed_f))  # feed forward 2 part
         fed_ff_d = keras.layers.Dropout(0.1)(fed_ff)
 
-        add2 = normalised + fed_ff_d
-        normalised2 = keras.layers.LayerNormalization()(add2)
+        add2 = normalized + fed_ff_d
+        normalized2 = keras.layers.LayerNormalization()(add2)
 
-        encoded = normalised2  # and the loop is repeated
+        encoded = normalized2  # and the loop is repeated
 
     encoder_output = encoded  # output from encoder
 
-    # decoder part
-    de_embed = keras.layers.Embedding(input_dim=decoder_vocab_len, output_dim=d_model)(decoder_input)
-    de_embed_pos = de_embed + possitional_encoding_decoder
+    # Decoder part
+    de_embed = keras.layers.Embedding(input_dim=decoder_vocab_len, output_dim=d_model, mask_zero=True)(decoder_input)
+    de_mask = de_embed._keras_mask
+
+    de_embed_pos = CustomSinePositionEncoding()(de_embed)
     de_embed_pos = keras.layers.Dropout(0.1)(de_embed_pos)
+
+    # combined_mask = CombinedMask()(de_mask)
 
     decoded = de_embed_pos
     for i in range(n):
-        self_attention = (keras.layers.MultiHeadAttention(num_heads,
-                                        key_dim,
-                                        dropout=0.1,
-                                        use_bias=True,
-                                        output_shape=(d_model,))
-                            (decoded, decoded, decoded))
+        self_attention = keras.layers.MultiHeadAttention(
+            num_heads,
+            key_dim,
+            value_dim=value_dim,
+            dropout=0.1,
+            use_bias=True)(decoded, decoded, decoded
+                           # , attention_mask=de_mask
+                           )
         self_attention_d = keras.layers.Dropout(0.1)(self_attention)
 
         add = decoded + self_attention_d
-        normalised1 = keras.layers.LayerNormalization()(add)
+        normalized1 = keras.layers.LayerNormalization()(add)
 
-        cross_attention = (keras.layers.MultiHeadAttention(num_heads,
-                                        key_dim,
-                                        dropout=0.1,
-                                        use_bias=True,
-                                        output_shape=(d_model,))
-                           (normalised1, encoder_output,encoder_output))
+        cross_attention = keras.layers.MultiHeadAttention(
+            num_heads,
+            key_dim,
+            value_dim=value_dim,
+            dropout=0.1,
+            use_bias=True)(normalized1, encoder_output, encoder_output
+                           # , attention_mask=encoder_mask
+                           )
         cross_attention_d = keras.layers.Dropout(0.1)(cross_attention)
 
-        add2 = normalised1 + cross_attention_d
-        normalised2 = keras.layers.LayerNormalization()(add2)
+        add2 = normalized1 + cross_attention_d
+        normalized2 = keras.layers.LayerNormalization()(add2)
 
-        fed_f = keras.layers.Dense(d_ff)(normalised2)  # feed forward 1 part
+        fed_f = keras.layers.Dense(d_ff)(normalized2)  # feed forward 1 part
         fed_ff = keras.layers.Dense(d_model)(keras.activations.relu(fed_f))  # feed forward 2 part
         fed_ff_d = keras.layers.Dropout(0.1)(fed_ff)
 
-        add3 = normalised2 + fed_ff_d
-        normalised3 = keras.layers.LayerNormalization()(add3)
+        add3 = normalized2 + fed_ff_d
+        normalized3 = keras.layers.LayerNormalization()(add3)
 
-        decoded = normalised3
+        decoded = normalized3
 
     decoder_dense_output = keras.layers.Dense(decoder_vocab_len, activation='softmax', name='decoder_output')(decoded)
 
@@ -104,8 +128,8 @@ if __name__ == '__main__':
     model.summary()
 
     # Generate random input data with appropriate shapes
-    encoder_input_data = np.random.randint(0, 10000, (1, 100))  # (batch_size, sequence_length)
-    decoder_input_data = np.random.randint(0, 10000, (1, 100))  # (batch_size, sequence_length)
+    encoder_input_data = np.random.randint(0, 10000, (1, 190))  # (batch_size, sequence_length)
+    decoder_input_data = np.random.randint(0, 10000, (1, 170))  # (batch_size, sequence_length)
 
     # Call the model with the random input data
     output = model.call([encoder_input_data, decoder_input_data], training=False)

d3e4ee46ce98a9bb7e643b199f85ba7e8970bb29
CasualMathEnjoyer <morovicsova.katka@gmail.com>
2025-03-04 11:16:44 +0100

    one function model pipeline

--- 2_en_de_transformer/s2s_transformer_pipeline.py
+++ 2_en_de_transformer/s2s_transformer_pipeline.py
@@ -11,135 +11,137 @@ from keras.utils import set_random_seed
 # from model_file_2 import *  # for loading
 from model_file_mine import *
 from model_function import save_model, load_model_mine, translate, get_epochs_train_accuracy, test_gpus
-from Data import Data
 from data_preparation import get_history_dict, join_dicts, load_cached_dict, cache_dict, create_new_class_dict, load_class_data, create_new_class_dict_testing
 
 print("Starting transform2seq")
 
-# settings json loading ---------------------------------------
-model_settings_path = 'model_settings.json'
-with open(model_settings_path, encoding="utf-8") as f:
-    model_settings = json.load(f)
+def run_model_pipeline(model_settings, model_compile_settings, run_settings):
+    # ---------------------------------------------------------------
+    all_models_path = run_settings["all_models_path"]
+    model_name_short = run_settings["model_name_short"]
 
-model_compile_settings_path = 'model_compile_settings.json'
-with open(model_compile_settings_path, encoding="utf-8") as f:
-    model_compile_settings = json.load(f)
+    result_json_path = f"json_results/transformer_results_{run_settings['version']}_{run_settings['testing_samples']}.json"
+    model_folder_path = os.path.join(all_models_path, model_name_short)
+    if not os.path.exists(model_folder_path): os.makedirs(model_folder_path)
+    model_full_path = os.path.join(model_folder_path, model_name_short)
+    history_dict = f"{model_full_path}_HistoryDict"
+    testing_cache_filename = model_full_path + '_TestingCache'
 
-run_settings_path = 'run_settings.json'
-with open(run_settings_path, encoding="utf-8") as f:
-    run_settings = json.load(f)
+    if run_settings["use_random_seed"]: a = random.randrange(0, 2**32 - 1)
+    else: a = run_settings["seed"]
+    set_random_seed(a)
 
-# ---------------------------------------------------------------
-all_models_path = run_settings["all_models_path"]
-model_name_short = run_settings["model_name_short"]
+    os.environ["KERAS_BACKEND"] = "tensorflow"
+    test_gpus()
 
-result_json_path = f"json_results/transformer_results_{run_settings['version']}_{run_settings['testing_samples']}.json"
-model_folder_path = os.path.join(all_models_path, model_name_short)
-if not os.path.exists(model_folder_path): os.makedirs(model_folder_path)
-model_full_path = os.path.join(model_folder_path, model_name_short)
-history_dict = f"{model_full_path}_HistoryDict"
-testing_cache_filename = model_full_path + '_TestingCache'
+    # ---------------------------- DATA PROCESSING -------------------------------------------------
+    if run_settings["new_class_dict"]: source, target, val_source, val_target = create_new_class_dict(run_settings)
+    else: source, target, val_source, val_target = load_class_data(run_settings)
 
-if run_settings["use_random_seed"]: a = random.randrange(0, 2**32 - 1)
-else: a = run_settings["seed"]
-set_random_seed(a)
+    # --------------------------------- MODEL ---------------------------------------------------------------------------
+    old_dict = get_history_dict(history_dict, run_settings["new_model"])
+    print("[MODEL] - starting")
+    if run_settings["new_model"]: model = model_func(source.vocab_size, target.vocab_size, source.maxlen, target.maxlen, model_settings)
+    else: model = load_model_mine(model_full_path)
 
-os.environ["KERAS_BACKEND"] = "tensorflow"
-test_gpus()
+    model.compile(optimizer=model_compile_settings['optimizer'],
+                  loss=model_compile_settings['loss'],
+                  metrics=model_compile_settings['metrics'])
 
-# ---------------------------- DATA PROCESSING -------------------------------------------------
-if run_settings["new_class_dict"]: source, target, val_source, val_target = create_new_class_dict(run_settings)
-else: source, target, val_source, val_target = load_class_data(run_settings)
+    print("[MODEL] - COMPILED")
+    # model.summary()
 
-# --------------------------------- MODEL ---------------------------------------------------------------------------
-old_dict = get_history_dict(history_dict, run_settings["new_model"])
-print("[MODEL] - starting")
-if run_settings["new_model"]: model = model_func(source.vocab_size, target.vocab_size, source.maxlen, target.maxlen, model_settings)
-else: model = load_model_mine(model_full_path)
-
-model.compile(optimizer=model_compile_settings['optimizer'],
-              loss=model_compile_settings['loss'],
-              metrics=model_compile_settings['metrics'])
-
-print("[MODEL] - COMPILED")
-# model.summary()
-
-if run_settings["finetune_model"]:
-    extend_model_embeddings(model, source.vocab_size, target.vocab_size)
-    model = adjust_output_layer(model, new_vocab_size=target.vocab_size)
-    print("[MODEL] - fine-tuning preparation finished")
-else:
-    print("[MODEL] - SKIPPING fine-tuning")
-
-# --------------------------------- TRAINING ------------------------------------------------------------------------
-if run_settings["train"]:
-    print("[TRAINING] - training started")
-    for i in range(run_settings["repeat"]):
-        history = model.fit(
-            (source.padded, target.padded), target.padded_shift_one,
-            batch_size=run_settings["batch_size"],
-            epochs = run_settings["epochs"],
-            validation_data=((val_source.padded, val_target.padded), val_target.padded_shift_one))
-
-        save_model(model, model_full_path)
-
-        new_dict = join_dicts(old_dict, history.history)
-        old_dict = new_dict
-        with open(history_dict, 'wb') as file_pi:
-            pickle.dump(new_dict, file_pi)
-
-        K.clear_session()
-    print("[TRAINING] - training finished")
-else:
-    print("[TRAINING] - SKIPPING")
-
-
-# ---------------------------------- TESTING ------------------------------------------------------------------------
-if run_settings["test"]:
-    test_source, test_target = create_new_class_dict_testing(run_settings, source, target)
-
-    print("[TESTING] - starting testing")
-    model = load_model_mine(model_full_path)
-
-    if run_settings["caching_in_testing"]:
-        print("[TESTING] - CACHE ON")
-        tested_dict = load_cached_dict(testing_cache_filename)
+    if run_settings["finetune_model"]:
+        extend_model_embeddings(model, source.vocab_size, target.vocab_size)
+        model = adjust_output_layer(model, new_vocab_size=target.vocab_size)
+        print("[MODEL] - fine-tuning preparation finished")
     else:
-        print("[TESTING] - CACHE OFF")
+        print("[MODEL] - SKIPPING fine-tuning")
 
-    # Testing Loop
-    output = []
-    for j in tqdm(range(len(test_source.padded))):
-        i = 1
-        encoder_input = np.array([test_source.padded[j]])
+    # --------------------------------- TRAINING ------------------------------------------------------------------------
+    if run_settings["train"]:
+        print("[TRAINING] - training started")
+        for i in range(run_settings["repeat"]):
+            history = model.fit(
+                (source.padded, target.padded), target.padded_shift_one,
+                batch_size=run_settings["batch_size"],
+                epochs = run_settings["epochs"],
+                validation_data=((val_source.padded, val_target.padded), val_target.padded_shift_one))
+
+            save_model(model, model_full_path)
+
+            new_dict = join_dicts(old_dict, history.history)
+            old_dict = new_dict
+            with open(history_dict, 'wb') as file_pi:
+                pickle.dump(new_dict, file_pi)
+
+            K.clear_session()
+        print("[TRAINING] - training finished")
+    else:
+        print("[TRAINING] - SKIPPING")
+
+
+    # ---------------------------------- TESTING ------------------------------------------------------------------------
+    if run_settings["test"]:
+        test_source, test_target = create_new_class_dict_testing(run_settings, source, target)
+
+        print("[TESTING] - starting testing")
+        model = load_model_mine(model_full_path)
+
         if run_settings["caching_in_testing"]:
-            encoder_cache_code = tuple(encoder_input[0])  # cos I can't use np array or list as a hash, [0] removes [around]
-            if encoder_cache_code in tested_dict:
-                output_line = tested_dict[encoder_cache_code]
+            print("[TESTING] - CACHE ON")
+            tested_dict = load_cached_dict(testing_cache_filename)
+        else:
+            print("[TESTING] - CACHE OFF")
+
+        # Testing Loop
+        output = []
+        for j in tqdm(range(len(test_source.padded))):
+            i = 1
+            encoder_input = np.array([test_source.padded[j]])
+            if run_settings["caching_in_testing"]:
+                encoder_cache_code = tuple(encoder_input[0])  # cos I can't use np array or list as a hash, [0] removes [around]
+                if encoder_cache_code in tested_dict:
+                    output_line = tested_dict[encoder_cache_code]
+                else:
+                    output_line = translate(model, encoder_input, target.maxlen, j)
+                    tested_dict[encoder_cache_code] = output_line
             else:
                 output_line = translate(model, encoder_input, target.maxlen, j)
-                tested_dict[encoder_cache_code] = output_line
-        else:
-            output_line = translate(model, encoder_input, target.maxlen, j)
-        output.append(output_line)
+            output.append(output_line)
 
-    if run_settings["caching_in_testing"]:
-        cache_dict(tested_dict, testing_cache_filename)
+        if run_settings["caching_in_testing"]:
+            cache_dict(tested_dict, testing_cache_filename)
 
-    # PRETTY TESTING PRINTING
-    from testing_s2s import test_translation, add_to_json
+        # PRETTY TESTING PRINTING
+        from testing_s2s import test_translation, add_to_json
 
-    rev_dict = test_target.create_reverse_dict(test_target.dict_chars)
-    valid = list(test_target.padded.astype(np.int32))
+        rev_dict = test_target.create_reverse_dict(test_target.dict_chars)
+        valid = list(test_target.padded.astype(np.int32))
 
-    dict = test_translation(output, valid, rev_dict, run_settings["sep"], run_settings["mezera"], use_custom_rules=False)
+        dict = test_translation(output, valid, rev_dict, run_settings["sep"], run_settings["mezera"], use_custom_rules=False)
 
-    all_epochs, training_data = get_epochs_train_accuracy(history_dict)
+        all_epochs, training_data = get_epochs_train_accuracy(history_dict)
 
-    add_to_json(result_json_path, model_name_short, dict, run_settings["testing_samples"],
-                all_epochs, training_data, run_settings["keras_version"])
-    print("[TESTING] - FINISHED")
-    print(f"[TESTING] - Saved to json for model: {model_name_short}")
-    print()
-else:
-    print("[TESTING] - SKIPPING Testing")
+        add_to_json(result_json_path, model_name_short, dict, run_settings["testing_samples"],
+                    all_epochs, training_data, run_settings["keras_version"])
+        print("[TESTING] - FINISHED")
+        print(f"[TESTING] - Saved to json for model: {model_name_short}")
+        print()
+    else:
+        print("[TESTING] - SKIPPING Testing")
+
+if __name__ == "__main__":
+    model_settings_path = 'model_settings.json'
+    with open(model_settings_path, encoding="utf-8") as f:
+        model_settings = json.load(f)
+
+    model_compile_settings_path = 'model_compile_settings.json'
+    with open(model_compile_settings_path, encoding="utf-8") as f:
+        model_compile_settings = json.load(f)
+
+    run_settings_path = 'run_settings.json'
+    with open(run_settings_path, encoding="utf-8") as f:
+        run_settings = json.load(f)
+
+    run_model_pipeline(model_settings, model_compile_settings, run_settings)

f5c71e8e3d950a67e20d0f5e674e3ba3abd93d2b
CasualMathEnjoyer <morovicsova.katka@gmail.com>
2025-03-04 11:12:22 +0100

    cleaned code

--- 2_en_de_transformer/s2s_transformer_pipeline.py
+++ 2_en_de_transformer/s2s_transformer_pipeline.py
@@ -8,7 +8,6 @@ from tqdm import tqdm
 from keras import backend as K
 from keras.utils import set_random_seed
 
-
 # from model_file_2 import *  # for loading
 from model_file_mine import *
 from model_function import save_model, load_model_mine, translate, get_epochs_train_accuracy, test_gpus
@@ -29,40 +28,17 @@ with open(model_compile_settings_path, encoding="utf-8") as f:
 run_settings_path = 'run_settings.json'
 with open(run_settings_path, encoding="utf-8") as f:
     run_settings = json.load(f)
-# ---------------------------------------------------------------
 
-# Access loaded settings
-new = run_settings["new_model"]
-new_class_dict = run_settings["new_class_dict"]
-caching_in_testing = run_settings["caching_in_testing"]
-batch_size = run_settings["batch_size"]
-epochs = run_settings["epochs"]
-repeat = run_settings["repeat"]
+# ---------------------------------------------------------------
 all_models_path = run_settings["all_models_path"]
 model_name_short = run_settings["model_name_short"]
-finetune_model = run_settings["finetune_model"]
-finetune_source = run_settings["finetune_source"]
-finetune_tgt = run_settings["finetune_tgt"]
-testing_samples = run_settings["testing_samples"]
-use_custom_testing = run_settings["use_custom_testing"]
-custom_test_src = run_settings["custom_test_src"]
-custom_test_tgt = run_settings["custom_test_tgt"]
-version = run_settings["version"]
-keras_version = run_settings["keras_version"]
-class_data = run_settings["class_data"]
 
-train_in_file_name = run_settings["train_in_file_name"]
-train_out_file_name = run_settings["train_out_file_name"]
-val_in_file_name = run_settings["val_in_file_name"]
-val_out_file_name = run_settings["val_out_file_name"]
-test_in_file_name = run_settings["test_in_file_name"]
-test_out_file_name = run_settings["test_out_file_name"]
-
-# dynamicaly change for namings
-result_json_path = f"json_results/transformer_results_{version}_{testing_samples}.json"
-model_full_path = os.path.join(all_models_path, model_name_short)
+result_json_path = f"json_results/transformer_results_{run_settings['version']}_{run_settings['testing_samples']}.json"
+model_folder_path = os.path.join(all_models_path, model_name_short)
+if not os.path.exists(model_folder_path): os.makedirs(model_folder_path)
+model_full_path = os.path.join(model_folder_path, model_name_short)
 history_dict = f"{model_full_path}_HistoryDict"
-
+testing_cache_filename = model_full_path + '_TestingCache'
 
 if run_settings["use_random_seed"]: a = random.randrange(0, 2**32 - 1)
 else: a = run_settings["seed"]
@@ -76,9 +52,9 @@ if run_settings["new_class_dict"]: source, target, val_source, val_target = crea
 else: source, target, val_source, val_target = load_class_data(run_settings)
 
 # --------------------------------- MODEL ---------------------------------------------------------------------------
-old_dict = get_history_dict(history_dict, new)
+old_dict = get_history_dict(history_dict, run_settings["new_model"])
 print("[MODEL] - starting")
-if new: model = model_func(source.vocab_size, target.vocab_size, source.maxlen, target.maxlen, model_settings)
+if run_settings["new_model"]: model = model_func(source.vocab_size, target.vocab_size, source.maxlen, target.maxlen, model_settings)
 else: model = load_model_mine(model_full_path)
 
 model.compile(optimizer=model_compile_settings['optimizer'],
@@ -88,7 +64,7 @@ model.compile(optimizer=model_compile_settings['optimizer'],
 print("[MODEL] - COMPILED")
 # model.summary()
 
-if finetune_model:
+if run_settings["finetune_model"]:
     extend_model_embeddings(model, source.vocab_size, target.vocab_size)
     model = adjust_output_layer(model, new_vocab_size=target.vocab_size)
     print("[MODEL] - fine-tuning preparation finished")
@@ -98,11 +74,11 @@ else:
 # --------------------------------- TRAINING ------------------------------------------------------------------------
 if run_settings["train"]:
     print("[TRAINING] - training started")
-    for i in range(repeat):
+    for i in range(run_settings["repeat"]):
         history = model.fit(
             (source.padded, target.padded), target.padded_shift_one,
-            batch_size=batch_size,
-            epochs=epochs,
+            batch_size=run_settings["batch_size"],
+            epochs = run_settings["epochs"],
             validation_data=((val_source.padded, val_target.padded), val_target.padded_shift_one))
 
         save_model(model, model_full_path)
@@ -123,11 +99,6 @@ if run_settings["test"]:
     test_source, test_target = create_new_class_dict_testing(run_settings, source, target)
 
     print("[TESTING] - starting testing")
-    model_full_path = os.path.join(all_models_path, model_name_short)
-    history_dict = model_full_path + '_HistoryDict'
-    testing_cache_filename = model_full_path + '_TestingCache'
-    print(f"[TESTING] - {model_full_path}")
-
     model = load_model_mine(model_full_path)
 
     if run_settings["caching_in_testing"]:
@@ -165,8 +136,8 @@ if run_settings["test"]:
 
     all_epochs, training_data = get_epochs_train_accuracy(history_dict)
 
-    add_to_json(result_json_path, model_name_short, dict, testing_samples,
-                all_epochs, training_data, keras_version)
+    add_to_json(result_json_path, model_name_short, dict, run_settings["testing_samples"],
+                all_epochs, training_data, run_settings["keras_version"])
     print("[TESTING] - FINISHED")
     print(f"[TESTING] - Saved to json for model: {model_name_short}")
     print()
